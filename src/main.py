b37346eea83257cd248c91dc188d771cdd02c8e8
DDataAly <alyona.dorof@gmail.com>
2025-08-03 10:10:19 +0100

    Adds logic to find a match between the update of the order book copy and ws_stream message upadating it to this state

--- src/main.py
+++ src/main.py
@@ -6,32 +6,79 @@ from utils.helpers import path_initial_shapshot
 
 
 uri = 'wss://stream.binance.com:9443/ws/btcusdt@depth'
-ws_buffer=[]
 
-
-async def ws_ingestion(queue, websocket):
-    # Infinite ingestion function
-    while True:
-        response = await websocket.recv() # The first response from the server is the subscription confirmation not the actual data
-        ws_stream = json.loads(response)
-        await queue.put(ws_stream)
-
-async def to_do_processing_logic(ws_stream):
-    pass
-
-async def ws_processing(queue):
-    # Infinite processing function
-    while True:
-        ws_stream =  await queue.get()
-        await to_do_processing_logic(ws_stream) #tbc it we should await here - depends on inner logic
-
-async def subscribe_websocket(websocket):
+async def send_subscription_request(websocket):
     await websocket.send(
         json.dumps({
             "method": "SUBSCRIBE",
             "params": ["btcusdt@depth@100ms"], 
             "id": 1}))
-    print ("Subscribed to the channel")
+    response = await websocket.recv()
+    return (response)
+
+
+async def is_subscription_confirmed(response) -> bool:
+    response_dict = json.loads(response)
+    if response_dict.keys() == {'result','id'}:
+        return True
+    elif response_dict.get('e') == "depthUpdate":
+        return True
+    return False
+
+
+async def ws_ingestion(queue, websocket):
+    # Infinite ingestion function
+    while True:
+        print ('Trying to ingest')
+        response = await websocket.recv() 
+        # print('Trying to parse')
+        # ws_stream = json.loads(response)
+        print('Trying to put in the queue')
+        await queue.put(response)
+
+
+
+async def is_ws_in_order_book(response, queue, replace_order_book = True, order_book_ts = None) -> bool:
+    parsed = json.loads(response)
+    ws_first_update_id = parsed['U']
+    ws_final_update_id = parsed ['u']
+    if replace_order_book:
+        order_book_ts = get_order_book()
+    is_aligned = False 
+
+    while is_aligned == False:
+        if ws_first_update_id <= order_book_ts < ws_final_update_id:
+            is_aligned = True
+    
+        if order_book_ts < ws_first_update_id:
+            print('Replacing order book')
+            return await is_ws_in_order_book(response, queue, replace_order_book = True)
+
+        if ws_final_update_id <= order_book_ts:
+            print('Requesting a new ws')
+            response =  await queue.get() 
+            return await is_ws_in_order_book(response, queue, replace_order_book = False, order_book_ts=order_book_ts)     
+    return is_aligned
+
+
+async def to_do_processing_logic():
+    pass
+
+
+async def ws_processing(queue):
+    # Infinite processing function
+    continue_search = True
+    while continue_search:
+        ws_stream =  await queue.get()
+        is_aligned = await is_ws_in_order_book(ws_stream, queue)
+        print (is_aligned)
+        if is_aligned == True:
+            print ('The match is found')
+            continue_search = False
+        else:
+            continue
+
+    await to_do_processing_logic()
 
 
 async def run_code():
@@ -39,7 +86,10 @@ async def run_code():
         async with websockets.connect(uri) as websocket:
             print("Connected to server")
             #Run outside the queue to guarantee the ordering
-            await subscribe_websocket(websocket)
+            response = await send_subscription_request(websocket)
+            if await is_subscription_confirmed(response):
+                print ('Subscription is confirmed')
+
 
             queue =  asyncio.Queue()
             ws_ingestion_task = asyncio.create_task(ws_ingestion(queue,websocket))
@@ -70,16 +120,18 @@ async def run_code():
         print("WebSocket connection closed")
 
 
-asyncio.run(run_code())
 
-
-def get_order_book():
+def get_order_book() -> int:
     snapshot = requests.get('https://api.binance.com/api/v3/depth?symbol=BTCUSDT&limit=20').json()
     with open ((path_initial_shapshot), 'w') as file:
         json.dump(snapshot, file)
     print('Order book is saved')
+    order_book_timestamp = snapshot.get("lastUpdateId")
+    return order_book_timestamp
 
 get_order_book()     
+
+asyncio.run(run_code())
 
 
 
@@ -93,15 +145,7 @@ get_order_book()
 # # Impement cheksum func to ensure the correctness of the local order book
 
 
-            # subscr_confirm_exp_keys = {'result','id'}
-            # if p.keys() == subscr_confirm_exp_keys:
-            #     print(f'Subscription is confirmed: {p}')
-            # elif p['e'] == "depthUpdate":
-            #     ws_buffer.append(response)
-            #     print (f'Data message sent before the subscription acknowledgement')
-            # else:
-            #     print('Wrong response from the server')
- 
+
 
 
 
