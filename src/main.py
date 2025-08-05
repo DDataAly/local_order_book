55e10a8b903835ca60bfaeaf4be122aff9285ee6
DDataAly <alyona.dorof@gmail.com>
2025-08-05 23:04:27 +0100

    Adds docstrings to 3 functions

--- src/main.py
+++ src/main.py
@@ -9,7 +9,35 @@ from collections import deque
 uri = 'wss://stream.binance.com:9443/ws/btcusdt@depth'
 
 
-async def send_subscription_request(websocket):
+async def send_subscription_request(websocket) -> str:
+    """
+    Sends a subscription request to the Binance WebSocket for depth updates.
+    Args:
+        websocket: The WebSocket connection to Binance.
+    Returns:
+        A JSON-formatted response string from Binance, which is either:
+        - subscription confirmation {"result": null, "id": 1}
+        - depth update message 
+            {
+            "e": "depthUpdate", // Event type
+            "E": 1672515782136, // Event time
+            "s": "BNBBTC",      // Symbol
+            "U": 157,           // First update ID in event
+            "u": 160,           // Final update ID in event
+            "b": [              // Bids to be updated
+                [
+                "0.0024",       // Price level to be updated
+                "10"            // Quantity
+                ]
+            ],
+            "a": [              // Asks to be updated
+                [
+                "0.0026",       // Price level to be updated
+                "100"           // Quantity
+                ]
+            ]
+            }
+    """
     await websocket.send(
         json.dumps({
             "method": "SUBSCRIBE",
@@ -20,13 +48,38 @@ async def send_subscription_request(websocket):
 
 
 async def is_subscription_confirmed(response) -> bool:
-    response_dict = json.loads(response)
+    """
+    Parses the response string and checks that expected dictionary keys are present 
+    Args:
+        response: string returned by send_subscription_request(websocket)
+    Returns:
+        bool - True if keys are present otherwise False   
+    """
+    try:
+        response_dict = json.loads(response)
+    except json.JSONDecodeError as e:
+        print (f'Invalid format of server response: {e}') 
+        return False  
+         
     if response_dict.keys() == {'result','id'}:
         return True
     elif response_dict.get('e') == "depthUpdate":
         return True
     return False
 
+async def ws_ingestion(websocket: websockets.WebSocketClientProtocol,buffer: deque[str]):
+    """
+    Indefinite function which receives order book prices and quantity updates and adds them to buffer
+    Args:
+        websocket: The WebSocket connection to Binance
+        buffer: deque to keep incoming steam messages
+    Returns:
+        Nothing 
+    """
+    while True:
+        print ('Continue ingestion')
+        response = await websocket.recv() 
+        buffer.append(response)
 
 async def get_first_message_id(buffer):
     while not buffer:
@@ -44,13 +97,6 @@ async def get_first_message_id(buffer):
         await asyncio.sleep(0.01)
 
 
-async def ws_ingestion(websocket,buffer):
-    # Infinite ingestion function
-    while True:
-        print ('Trying to ingest')
-        response = await websocket.recv() 
-        print('Trying to put in the buffer')
-        buffer.append(response)
 
 async def find_matching_messsage(order_book_last_update_id,buffer, event, match):
     while not event.is_set():
