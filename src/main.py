814119ced8bbf7feca8ad9e21079f5d3398af05d
DDataAly <alyona.dorof@gmail.com>
2025-08-24 11:25:34 +0100

    Fix the bug is ws_processing (previously missing json.loads for messages to process)

--- src/main.py
+++ src/main.py
@@ -3,8 +3,11 @@ import websockets
 import json
 import aiohttp
 from collections import deque
-from order_book.order_book_class import OrderBook, EmptyOrderBookException
+from order_book.order_book_class import OrderBook
 from utils.helpers import path_initial_shapshot
+
+class MissingMessageInIngestedStream(Exception):
+    """Raised when there is a gap between the update IDs in the stream of ingested depth updates"""   
 
 uri = 'wss://stream.binance.com:9443/ws/btcusdt@depth'
 
@@ -205,18 +208,39 @@ async def create_and_save_local_order_book(snapshot, order_book_last_update_id):
 
   
 async def to_do_processing_logic(order_book, message):
-    for message in buffer:
+        print('Trying to execute the logic')
         order_book.ob_bids, order_book.ob_asks = await order_book.update_order_book(message)
-        order_book.ob_bids_prices, order_book.ob_asks_prices = await order_book.update_price_lists(message)      
+        order_book.ob_bids_prices, order_book.ob_asks_prices = await order_book.update_price_lists(message)   
+        print('Processing is done')   
         await asyncio.sleep (0.1)
 
 
 async def ws_processing(order_book, buffer):
     # Infinite processing function
-    while buffer:
-        print ('Continue processing')
-        await to_do_processing_logic()
-        buffer.popleft()
+    while True:
+        if not buffer:
+            await asyncio.sleep(0.1)
+            continue
+
+        try:
+            print ('Continue processing')
+            message = json.loads(buffer.popleft())
+            print(f'Printing the message I am going to process: {message}. It has type {type(message)}')
+            await to_do_processing_logic(order_book, message)
+            print('Job done, let me check if the next message is valid...')
+            last_update_current_message = message['u']
+            first_update_next_message =  json.loads(buffer[0])['U']
+            print (f'Last update current {last_update_current_message}, first update next {first_update_next_message}')
+            if int(last_update_current_message) + 1 == first_update_next_message:
+                print("All good with the next message")
+            else:
+                print('There is some issue with the next message') 
+                raise MissingMessageInIngestedStream ("There is a gap between update IDs between the processed and following message. Buffer is not valid.") 
+                # Here should be some logic around re-fetching order book and doing sync again - perhaps as a part of the orchestrator or main loop      
+            await asyncio.sleep (0.1)
+        except Exception as e:
+            print(f'Something is wrong with processing: {e}')
+
     
 
 
@@ -249,13 +273,7 @@ async def orchestrator(websocket):
 
     order_book = await create_and_save_local_order_book(snapshot, order_book_last_update_id)
 
-    # order_book = OrderBook(snapshot) 
-    # print(f'Order book object has been initialised with order book with the last update ID {order_book_last_update_id}')   
-    # with open ((path_initial_shapshot), 'w') as file:
-    #     json.dump(snapshot, file)
-    # print('Order book copy is saved locally')
-
-    ws_processing_task = asyncio.create_task(ws_processing(buffer))
+    ws_processing_task = asyncio.create_task(ws_processing(order_book, buffer))
     return ws_ingestion_task, ws_processing_task, order_book
     
 
