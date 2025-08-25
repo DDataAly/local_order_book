76fe2c3455a349fffbe0daf4b1e8e1566f40cae9
DDataAly <alyona.dorof@gmail.com>
2025-08-25 21:32:26 +0100

    Add __init__.py to create a package

---
+++ src/wb_sockets/__init__.py
@@ -0,0 +1,4 @@
+from .subscribing import run_the_subscriber
+from .ingesting import ws_ingestion
+from .syncing import fetch_order_book_snapshot, find_matching_message
+from .processing import ws_processing
