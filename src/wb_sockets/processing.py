8920a314fb42adcd5c0024825ec6f2f0b2e50854
DDataAly <alyona.dorof@gmail.com>
2025-09-13 12:49:14 +0100

    Breaks ws_processing on two function to separate concerns; propagates errors to the orchestrator level

--- src/wb_sockets/processing.py
+++ src/wb_sockets/processing.py
@@ -21,9 +21,17 @@ async def to_do_processing_logic(order_book, message):
         order_book.ob_bids, order_book.ob_asks = await order_book.update_order_book(message)
         order_book.ob_bids_prices, order_book.ob_asks_prices = await order_book.update_price_lists(message)   
         print('Processing is done')   
-        await asyncio.sleep (0.1)
 
-# Need to refactor
+
+async def message_continuity_checker (message_to_process, buffer):
+    last_update_message_to_process = message_to_process['u']
+    first_update_next_message =  json.loads(buffer[0])['U']
+    print (f'Last update current {last_update_message_to_process}, first update next {first_update_next_message}')
+    if int(last_update_message_to_process) + 1 == first_update_next_message:
+        return True
+    return False
+   
+
 async def ws_processing(order_book, buffer):
     # Infinite processing function
     while True:
@@ -33,19 +41,21 @@ async def ws_processing(order_book, buffer):
 
         try:
             print ('Continue processing')
-            message = json.loads(buffer.popleft())
-            print(f'Printing the message I am going to process: {message}. It has type {type(message)}')
-            await to_do_processing_logic(order_book, message)
+            message_to_process = json.loads(buffer.popleft())
+            print(f'Printing the message I am going to process: {message_to_process}. It has type {type(message_to_process)}')
+            await to_do_processing_logic(order_book, message_to_process)
             print('Job done, let me check if the next message is valid...')
-            last_update_current_message = message['u']
-            first_update_next_message =  json.loads(buffer[0])['U']
-            print (f'Last update current {last_update_current_message}, first update next {first_update_next_message}')
-            if int(last_update_current_message) + 1 == first_update_next_message:
+
+            if await message_continuity_checker(message_to_process, buffer):
                 print("All good with the next message")
             else:
-                print('There is some issue with the next message') 
                 raise MissingMessageInIngestedStream ("There is a gap between update IDs between the processed and following message. Buffer is not valid.") 
-                # Here should be some logic around re-fetching order book and doing sync again - perhaps as a part of the orchestrator or main event loop      
             await asyncio.sleep (0.1)
+
+        except MissingMessageInIngestedStream as e: 
+            print(f'Continuity gap detected: {e}')
+            raise
+
         except Exception as e:
             print(f'Something is wrong with processing: {e}')
+            raise
