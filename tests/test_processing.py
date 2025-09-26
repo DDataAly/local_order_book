1cdd9847dbf7358d6b759deabb7f256af57b28a3
DDataAly <alyona.dorof@gmail.com>
2025-09-26 13:24:50 +0100

    Fixes tests for ws_processing with asyncio order_book fixture

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,4 +1,5 @@
 import pytest
+import pytest_asyncio
 import json
 import asyncio
 from collections import deque
@@ -17,8 +18,8 @@ def curr_msg():
     }
     return curr_msg
 
-@pytest.fixture
-def order_book():
+@pytest_asyncio.fixture
+async def order_book():
     content = {"lastUpdateId": 74105025813, 
                         "bids": [["113678.85000000", "7.25330000"], 
                                 ["113678.84000000", "0.77360000"]
@@ -28,6 +29,7 @@ def order_book():
                                 ]
                             }
     order_book = OrderBook(content)
+    await order_book.extract_order_book_bids_asks()
     return order_book
 
 class TestIsContinuous():
@@ -128,11 +130,42 @@ class TestIsContinuous():
 
         assert not await is_continuous (curr_msg, buffer) 
 
-# Need to change to assert on bids and asks directly as content doesn't change till the production stage
+
 class TestWsProcessing():
         @pytest.mark.describe('Tests to ensure ws_processing runs correctly')
         @pytest.mark.asyncio
-        async def test_updates_order_book_continuous_buffer(self,curr_msg, order_book):
+        async def test_updates_order_book_continuous_buffer_new_bids_asks(self,curr_msg, order_book):
+                next_msg = {"e":"depthUpdate",
+                        "E":1753786825814,
+                        "s":"BTCUSDT",
+                        "U":73652024513,
+                        "u":73652024517,
+                        "b":[["114300.00000000", "0.73150000"]],
+                        "a":[["108304.00000000","2.77750000"]]}
+                buffer = deque([json.dumps(curr_msg), json.dumps(next_msg)])
+
+                task = asyncio.create_task(ws_processing(order_book, buffer)) 
+                await asyncio.sleep(0.5)
+
+                assert order_book.ob_bids == {113678.85: 7.2533, 113678.84: 0.7736, 118300.0: 1.7315}
+                assert order_book.ob_asks =={113678.86 : 1.93563, 113900.35 : 0.13677, 118304.0:1.7775}
+
+                task.cancel()
+                with pytest.raises(asyncio.CancelledError):
+                        await task
+
+
+        @pytest.mark.asyncio
+        async def test_updates_order_book_continuous_buffer_update_bids_asks(self, order_book):
+                # Creates a buffer from individual WebSockets messages
+                curr_msg = {"e":"depthUpdate",
+                        "E":1753786825814,
+                        "s":"BTCUSDT",
+                        "U":73652024499,
+                        "u":73652024512,
+                        "b":[["113678.85000000", "2.00000000"]],
+                        "a":[["113900.35000000","0.00000000"]]
+                        }
                 next_msg = {"e":"depthUpdate",
                         "E":1753786825814,
                         "s":"BTCUSDT",
@@ -142,25 +175,14 @@ class TestWsProcessing():
                         "a":[["108304.00000000","2.77750000"]]}
                 buffer = deque([json.dumps(curr_msg), json.dumps(next_msg)])
                 
-                order_book.ob_bids, _ = await order_book.extract_order_book_bids_asks()
-                print(f'This is ob.bids before: {order_book.ob_bids}')
-
                 task = asyncio.create_task(ws_processing(order_book, buffer)) 
                 await asyncio.sleep(0.5)
-                print(f'This is ob.bids after: {order_book.ob_bids}')
-                
-                assert order_book.content == {"lastUpdateId": 73652024512, 
-                        "bids": [["113678.85000000", "7.25330000"], 
-                                ["113678.84000000", "0.77360000"],
-                                ["118300.00000000", "1.73150000"]
-                            ], 
-                        "asks": [["113678.86000000", "1.93563000"], 
-                                ["113900.35000000", "0.13677000"],
-                                ["118304.00000000","1.77750000"]
-                                ]
-                            }
-                
+
+                assert order_book.ob_bids == {113678.85: 2.0, 113678.84: 0.7736}
+                assert order_book.ob_asks =={113678.86 : 1.93563}
+
                 task.cancel()
                 with pytest.raises(asyncio.CancelledError):
                         await task
+                                
                 
