5a16f36ed84baceabbc37553c90d0af57ec07b0a
DDataAly <alyona.dorof@gmail.com>
2026-05-10 13:11:38 +0100

    Change the exception order in get_order_book()

--- src/wb_sockets/syncing.py
+++ src/wb_sockets/syncing.py
@@ -45,11 +45,11 @@ async def get_order_book() -> tuple [dict, int]:
             async with session.get('https://api.binance.com/api/v3/depth?symbol=BTCUSDT&limit=20') as response:
                 response.raise_for_status()
                 snapshot = await response.json()
-        except aiohttp.ClientError as e:
-            print (f'Error fetching the order book snapshot: {e}') 
-            raise
         except aiohttp.ContentTypeError as e:
             print(f'The server response file is not a valid json: {e}') 
+            raise
+        except aiohttp.ClientError as e:
+            print (f'Error fetching the order book snapshot: {e}') 
             raise
         except Exception as e:
             print(f'An error occurred fetching the order book copy: {e}')
