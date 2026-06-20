fcbcbff4ae18b200a63d73aaaa89a6076a276d3f
DDataAly <alyona.dorof@gmail.com>
2026-06-20 19:20:02 +0100

    Split snapshot saving and order book object creation on individual functions

--- src/order_book/order_book_production.py
+++ src/order_book/order_book_production.py
@@ -2,28 +2,59 @@ from order_book.order_book_class import OrderBook
 import json
 import os
 
-async def create_and_save_local_order_book(snapshot, order_book_last_update_id):
-    order_book = OrderBook(snapshot) 
-    order_book.ob_bids, order_book.ob_asks = await order_book.extract_order_book_bids_asks()
-    order_book.ob_bids_prices, order_book.ob_asks_prices =  await order_book.extract_order_book_prices()
-    print(f'Order book object has been initialised with order book with the last update ID {order_book_last_update_id}')   
+async def create_order_book(snapshot) -> OrderBook:
+    try: 
+        save_snapshot(snapshot)
+    except Exception:
+        print ("Error saving initial snapshot")
     
-    # This returns path to local_order_book directory itself using path to order_book_production.py and moving up
+    try:
+        order_book = OrderBook(snapshot) 
+        order_book.ob_bids, order_book.ob_asks = await order_book.extract_order_book_bids_asks()
+        order_book.ob_bids_prices, order_book.ob_asks_prices =  await order_book.extract_order_book_prices()
+        print(f'Order book object has been created based on the snapshot with the last update ID {snapshot["lastUpdateId"]}')   
+        return order_book
+    except Exception:
+        print('An error occurred creating order book object')
+        raise
+
+
+def save_snapshot(snapshot: dict) -> None:
+    # Find the project directory path 
     PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-    print(f"This is the path: {os.path.abspath(__file__)}") # This returns path to main.py since we call this func from main.py
     print(f"This is the project directory: {PROJECT_DIR}")
+
+    # Create data folder in the project directory if doesn't exist
     snapshot_directory = os.path.join(PROJECT_DIR, "data")
-    os.makedirs(snapshot_directory, exist_ok = True) # This checks if the data directory exists and creates it if it doesn't
+    os.makedirs(snapshot_directory, exist_ok = True)
+    print(f"This is the path to the directory where we want to save the snapshot: {snapshot_directory}") 
 
-    print(f"This is the full snapshot path: {snapshot_directory}") 
+    # Create path to the file where we save the snapshot to
     full_file_path = os.path.join(snapshot_directory, 'ob_initial_snapshot.json')
-    print(f"This is the actual absolute path of the output: {full_file_path}")
-
+    print(f"This is the path of the file where we will write the snapshot to: {full_file_path}")
     with open ((full_file_path), 'w') as file:
         json.dump(snapshot, file)
-    print('Order book copy is saved locally')
-
-    return order_book
+    print('Initial snapshot of the order book is saved locally')
 
 
 
+
+# async def save_local_copy_ready_for_validation(order_book):
+#     await order_book.prepare_local_copy_for_validation(order_book)
+
+# def inner_layer():
+#     raise ValueError
+
+# def mid_layer():
+#     print('cats')
+#     inner_layer()
+
+# def outer_layer():
+#     print ("Fabulous")
+#     try:
+#         mid_layer()
+#     except Exception as e:
+#         print('Here we are!')
+#         raise e
+
+# outer_layer()
