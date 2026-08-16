548c6ba4477df6e335e87f47e401f5391f8c8014
DDataAly <alyona.dorof@gmail.com>
2026-08-16 19:51:46 +0100

    Enhance validate_snapshot function

--- src/wb_sockets/syncing.py
+++ src/wb_sockets/syncing.py
@@ -51,17 +51,23 @@ async def get_order_book() -> dict:
             raise
     return snapshot
 
+
+
 def validate_snapshot(snapshot: dict) -> bool:
     try:
         assert all([item in snapshot.keys() for item in ['lastUpdateId', 'asks', 'bids']])
-        assert all([float(snapshot[side][0][0]) for side in['bids', 'asks']])
-        assert all([float(snapshot[side][0][1]) for side in['bids', 'asks']])
+        assert all(len(snapshot[key])!=0 for key in ['asks', 'bids'])
+        assert isinstance(snapshot['lastUpdateId'], int) 
+        assert snapshot['lastUpdateId']!= 0
+        
+        for side in ['bids', 'asks']:
+            for price,qty in snapshot[side]:
+                assert not(float(qty) == 0 or float(price) == 0)
+        print('All good!')
     except Exception as e:
         print("Invalid snapshot received, retrying")
         return False
     return True
-
-
 
 
 async def fetch_order_book_snapshot(buffer) -> dict:
@@ -135,4 +141,85 @@ async def find_matching_message(order_book_last_update_id, buffer) -> None:
                 buffer.popleft()
         print('No matching message found in the buffer yet.')
 
+
+
+# sandbox
+# Original function:
+# def validate_snapshot(snapshot: dict) -> bool:
+    # try:
+    #     assert all([item in snapshot.keys() for item in ['lastUpdateId', 'asks', 'bids']])
+    #     assert all([float(snapshot[side][0][0]) for side in['bids', 'asks']])
+    #     assert all([float(snapshot[side][0][1]) for side in['bids', 'asks']])
+    # except Exception as e:
+    #     print("Invalid snapshot received, retrying")
+    #     return False
+    # return True
+
+# def validation_practice(snapshot:dict) -> bool:
+#     for side in ['bids', 'asks']:
+#         for item in snapshot[side]:
+#             print(float(item[0]))
+#     for side in ['bids', 'asks']:
+#         for item in snapshot[side]:
+#             print(float(item[1]))
+
+# def validation_practice2(snapshot: dict) -> bool:
+#     try: 
+#         assert all ([float(item[1]) for item in snapshot[side]] for side in ['bids', 'asks'])
+#         print('happy cats')
+#     except Exception:
+#         print('Invalid snapshot received')    
+
+# test_snapshot = {"lastUpdateId": 123, 
+#                  "bids": [['1.0','1.0'], ['1.1', '2.1']], 
+#                  'asks': [['4.1', '4.0'], ['3.1', '3.0']]}
+
+# # validation_practice(test_snapshot)
+# print('cats')
+# # validation_practice2(test_snapshot)
+
+# def validation_practice3(snapshot:dict) -> bool:
+#     for side in ['bids', 'asks']:
+#         for item in snapshot[side]:
+#             if float(item[0])!=0 and float(item[1])!=0:
+#                 print(True)
+#             else:
+#                 print (False)
+
+# # validation_practice3(test_snapshot)
+
+# def validation_practice4(snapshot:dict):
+#     try:
+#         assert all([True if (float(item[0])!=0 and float(item[1])!=0) else False for item in snapshot[side]] for side in ['bids', 'asks'])
+#         print ('Valid')
+#     except Exception:
+#         print ('Not valid')
+
+# # validation_practice4(test_snapshot)
+
+# def validation_practice5(snapshot:dict) -> bool:
+#     new_list=[]
+#     for side in ['bids', 'asks']:
+#         for qty,price in snapshot[side]:
+#             if float(qty) ==0 or float(price) ==0:
+#                 new_list.append('zero value found')
+#                 assert len(new_list) == 0
+
+# # validation_practice5(test_snapshot)
+
+# def validation_practice6(snapshot:dict) -> bool:
+#     for side in ['bids', 'asks']:
+#         for price,qty in snapshot[side]:
+#             assert not(float(qty) ==0 or float(price) ==0)
+
+# def validation_practice7(snapshot):
+#     assert all ((float(item[0])!=0 and float(item[1])!=0) for side in ['bids', 'asks'] for item in snapshot[side])
+#     print('Wohoo!')
+
+# test_snapshot = {"lastUpdateId": 123, 
+#                  "bids": [['1.0','1.0'], ['1.1', '2.1']], 
+#                  'asks': [['emma', '4.0'], ['3.1', '3.0']]}
+
+
+# validation_practice7(test_snapshot)
 
